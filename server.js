924c496b7184b5429a0684d6d1d40cca153afa2a
Chris Constantine <chris@omadahealth.com>
2012-05-02 18:52:22 -0700

    better channel management and setup for ssl

--- server.js
+++ server.js
@@ -2,55 +2,57 @@
 var net     = require('net');
 var express = require('express');
 
-var app     = express.createServer();
-
 var fs = require('fs');
-    
-app.configure(function () {
-                app.use(express.static(__dirname + '/public'));
-                app.use(express.cookieParser());
-                app.use(express.bodyParser());
-                app.use(app.router);
-              }); 
+function build(app) {
+  
+  app.configure(function () {
+                  app.use(express.static(__dirname + '/public'));
+                  app.use(express.cookieParser());
+                  app.use(express.bodyParser());
+                  app.use(app.router);
+                }); 
+  
+  app.get('/test',
+          function(req, res) {
+            console.log("GET: " + req.url); 
+            var s = new Buffer(req.cookies._kairos_session_.split("--")[0], 'base64').toString('ascii');
+            console.log(s);
+            res.json({var: s});
+          });
+  
+  var io = require('socket.io').listen(app);
+  
+  var redis = require("redis"),
+  client = redis.createClient(),
+  publisher = redis.createClient();
+  
+  client.subscribe("events");
+  client.on(
+    "message",
+    function(channel, message) {
+      console.log(channel, message);
+      data = JSON.parse(message);
+      io.sockets.emit(data.channel, data.message);
 
-app.get('/',
-        function(req, res) {
-          console.log("GET: " + req.url); 
-          var s = new Buffer(req.cookies._peakpickup_session.split("--")[0], 'base64').toString('ascii');
-          console.log(s);
-          res.end('var: ');
+    });
+
+  io.sockets.on(
+    'connection',
+    function (socket) {
+      socket.join('clicky'); 
+      socket.on(
+        'clicky',
+        function(data) {
+          console.log('clicky', data);
+          publisher.publish("events", "clicky-redis");
         });
+    });
+  
 
-var io = require('socket.io').listen(app);
+  return app;
+}  
 
-var redis = require("redis"),
-    client = redis.createClient();
-    publisher = redis.createClient();
-
-client.subscribe("events");
-io.sockets.on(
-  'connection',
-  function (socket) {
-    socket.join('clicky'); 
-    socket.on(
-      'clicky',
-      function(data) {
-        console.log('clicky', data);
-        publisher.publish("events", "clicky-redis");
-      });
-  });
-
-client.on(
-  "message",
-  function(channel, message) {
-    io.sockets.emit("events", 'redis click');
-    console.log(channel, message);
-});
-
+var app     = express.createServer();
+build(app).listen(80);
 app.listen(80);
 
-setInterval(
-  function() {
-    io.sockets.emit("events", 'fake');
-              
-}, 1000);
