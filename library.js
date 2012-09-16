1fd4af6b4883c92084026d28409207c794f4c25e
Chris Constantine <cconstan@gmail.com>
2012-09-16 14:22:42 -0700

    Export client code too

--- library.js
+++ library.js
@@ -2,3 +2,4 @@ require('coffee-script')
 
 this.Appliance = require('./lib/appliance').Appliance
 this.Emitter   = require('./lib/emitter').Emitter
+this.Client   = require('./assets/js/nodevent.coffee').NoDeventController
